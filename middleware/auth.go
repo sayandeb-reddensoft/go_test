619165d37154c007e4fd4e00099cee09929a29b3
muthukumar-u <muthu@nelsonin.com>
2025-07-02 13:50:53 +0530

    chore: add application middlewares

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,13 +1,92 @@
 package middleware
 
 import (
+	"fmt"
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
-	"github.com/nelsonin-research-org/clenz-auth/services"
+	constants "github.com/nelsonin-research-org/cdc-auth/const"
+	"github.com/nelsonin-research-org/cdc-auth/globals"
+	"github.com/nelsonin-research-org/cdc-auth/interfaces"
+	"github.com/nelsonin-research-org/cdc-auth/message"
+	"github.com/nelsonin-research-org/cdc-auth/utils"
 )
 
-func AuthMiddleware(userService *services.UserServiceImpl) gin.HandlerFunc {
+func AuthMiddleware(userService interfaces.UserService) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, message.ReturnMessage(http.StatusUnauthorized))
+			c.Abort()
+			return
+		}
+
+		parts := strings.Fields(authHeader)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusBadRequest, message.ReturnMessage(http.StatusBadRequest))
+			c.Abort()
+			return
+		}
+
+		tokenString := parts[1]
+
+		if globals.AppKeys.PrivateKey == nil || globals.AppKeys.PublicKeyPem == nil {
+			fmt.Println("pem keys not loaded")
+			c.JSON(http.StatusInternalServerError, message.ReturnMessage(http.StatusInternalServerError))
+			c.Abort()
+			return
+		}
+
+		claims, err := utils.ParseToken(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, message.ReturnCustomMessage("invalid token"))
+			c.Abort()
+			return
+		}
 		
+		tokenTypeRaw, ok := claims["token_type"]
+		if !ok {
+			c.JSON(http.StatusBadRequest, message.ReturnCustomMessage("token type not found in token"))
+			c.Abort()
+			return
+		}
+		
+		tokenTypeFloat, ok := tokenTypeRaw.(float64)
+		if !ok {
+			c.JSON(http.StatusBadRequest, message.ReturnCustomMessage("invalid token type format"))
+			c.Abort()
+			return
+		}
+		
+		if int(tokenTypeFloat) != constants.PRIMARY_TOKEN && int(tokenTypeFloat) != constants.REFRESH_TOKEN {
+			c.JSON(http.StatusBadRequest, message.ReturnCustomMessage("token type not matched"))
+			c.Abort()
+			return
+		}
+
+		userId, _ := userService.GetUserIdAndPasswordByEmail(claims["email"].(string))
+		if userId == "" {
+			c.JSON(http.StatusUnprocessableEntity, message.ReturnCustomMessage("acount not found"))
+			return
+		}
+				
+		verified, err := userService.IsUserAccountVerifiedByEmail(claims["email"].(string))
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, message.ReturnMessage(http.StatusInternalServerError))
+			c.Abort()
+			return
+		}
+		
+		if !verified {
+			c.JSON(http.StatusProxyAuthRequired, message.ReturnCustomMessage("acount not verified"))
+			c.Abort()
+			return
+		}
+
+		c.Set("id", claims["id"])
+		c.Set("email", claims["email"])
+		c.Set("role", claims["role"])
 		c.Next()
 	}
 }
--- middleware/tempAuth.go
+++ middleware/tempAuth.go
@@ -1,13 +1,87 @@
 package middleware
 
 import (
+	"fmt"
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
-	"github.com/nelsonin-research-org/clenz-auth/services"
+	constants "github.com/nelsonin-research-org/cdc-auth/const"
+	"github.com/nelsonin-research-org/cdc-auth/globals"
+	"github.com/nelsonin-research-org/cdc-auth/interfaces"
+	"github.com/nelsonin-research-org/cdc-auth/message"
+	"github.com/nelsonin-research-org/cdc-auth/utils"
 )
 
-func ValidateTempToken(userService *services.UserServiceImpl) gin.HandlerFunc {
+func ValidateTempToken(userService interfaces.UserService) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			fmt.Println("Authorization header not exist")
+			c.JSON(http.StatusUnauthorized, message.ReturnMessage(http.StatusUnauthorized))
+			c.Abort()
+			return
+		}
+
+		parts := strings.Fields(authHeader)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			fmt.Println("toekn not exist on header or invalid header")
+			c.JSON(http.StatusBadRequest, message.ReturnMessage(http.StatusBadRequest))
+			c.Abort()
+			return
+		}
+
+		tokenString := parts[1]
+
+		if globals.AppKeys.PrivateKey == nil || globals.AppKeys.PublicKeyPem == nil {
+			fmt.Println("pem keys not loaded")
+			c.JSON(http.StatusInternalServerError, message.ReturnMessage(http.StatusInternalServerError))
+			c.Abort()
+			return
+		}
+
+		claims, err := utils.ParseToken(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, message.ReturnCustomMessage("invalid token"))
+			c.Abort()
+			return
+		}
+
+		tokenTypeRaw, ok := claims["token_type"]
+		if !ok {
+			c.JSON(http.StatusBadRequest, message.ReturnCustomMessage("token type not found in token"))
+			c.Abort()
+			return
+		}
+
+		tokenTypeFloat, ok := tokenTypeRaw.(float64)
+		if !ok {
+			c.JSON(http.StatusBadRequest, message.ReturnCustomMessage("invalid token type format"))
+			c.Abort()
+			return
+		}
+
+		if int(tokenTypeFloat) != constants.TEMP_TOKEN {
+			c.JSON(http.StatusBadRequest, message.ReturnCustomMessage("token type not matched"))
+			c.Abort()
+			return
+		}
 		
+		exists, err := userService.IsUserAlreadyExists(claims["email"].(string))
+		if err != nil {
+			fmt.Println("error verifying user already exist or not")
+			c.JSON(http.StatusInternalServerError, message.ReturnMessage(http.StatusInternalServerError))
+			c.Abort()
+			return
+		}
+
+		if !exists {
+			c.JSON(http.StatusUnprocessableEntity, message.ReturnCustomMessage("user not found"))
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims["email"])
 		c.Next()
 	}
 }
