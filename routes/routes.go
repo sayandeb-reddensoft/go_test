b258b3d72963a3777a7fb254609528df30bcfa9e
muthukumar-u <muthu@nelsonin.com>
2025-07-02 13:49:26 +0530

    chore: add application handlers config

--- routes/routes.go
+++ routes/routes.go
@@ -2,20 +2,21 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	app "github.com/nelsonin-research-org/clenz-auth/handlers/data"
-	"github.com/nelsonin-research-org/clenz-auth/middleware"
+	app "github.com/nelsonin-research-org/cdc-auth/handlers/data"
+	"github.com/nelsonin-research-org/cdc-auth/middleware"
+	limitations "github.com/nelsonin-research-org/cdc-auth/models/limitation"
 )
 
-func NoAuthGroupRoutes(r *gin.RouterGroup, handlers app.AppHandlers) {
+func NoAuthGroupRoutes(r *gin.RouterGroup, handlers *app.AppHandlers) {
 	// user
 	r.POST("/login", handlers.UserHandler.Login)
-	r.POST("/sign-up", handlers.UserHandler.SignUp)
+	r.POST("/org/create", handlers.UserHandler.CreateOrganization)
 	r.POST("/verify/OTP", handlers.UserHandler.VerifyOtp)
-	r.POST("/resend/OTP", middleware.RateLimitMiddleware(5), handlers.UserHandler.ResendOTP)
-	r.POST("/forget-password", middleware.RateLimitMiddleware(10), handlers.UserHandler.ForgetPassword)
-	r.PUT("/password", middleware.ValidateTempToken(&handlers.UserHandler.UserService), middleware.RateLimitMiddleware(5), handlers.UserHandler.UpdatePassword)
-	r.POST("/account/logout", middleware.AuthMiddleware(&handlers.UserHandler.UserService), handlers.UserHandler.Logout)
+	r.POST("/resend/OTP", middleware.RateLimitMiddleware(limitations.HANDLER_LIMITATION.RESEND_OTP), handlers.UserHandler.ResendOTP)
+	r.POST("/forget-password", middleware.RateLimitMiddleware(limitations.HANDLER_LIMITATION.FORGOT_PASSWORD), handlers.UserHandler.ForgetPassword)
+	r.PUT("/password", middleware.ValidateTempToken(handlers.UserHandler.UserService), middleware.RateLimitMiddleware(limitations.HANDLER_LIMITATION.UPDATE_PASSWORD), handlers.UserHandler.UpdatePassword)
+	r.POST("/account/logout", middleware.AuthMiddleware(handlers.UserHandler.UserService), handlers.UserHandler.Logout)
 
 	// token
-	r.GET("/refresh-token", middleware.AuthMiddleware(&handlers.UserHandler.UserService), handlers.UserHandler.RefreshTokenHandler)
+	r.GET("/refresh-token", middleware.AuthMiddleware(handlers.UserHandler.UserService), handlers.UserHandler.RefreshTokenHandler)
 }
